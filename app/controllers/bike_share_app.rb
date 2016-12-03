d7ea31bfaaf0175837db9ba05b23618a3c660a71
Brett Rivers <riverswb@gmail.com>
2016-12-03 14:06:11 -0700

    had to reset my local git repo

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,18 @@
 class BikeShareApp < Sinatra::Base
+  get '/conditions' do
+    erb :"conditions/index"
+  end
+
+  get '/conditions/new' do
+
+  end
+
+  get '/conditions/:id' do
+    erb :show
+  end
+
+  get '/conditions/:id/edit' do
+
+  end
 
 end

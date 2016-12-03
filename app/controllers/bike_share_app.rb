60824a08db83986abc60c58c6199179ab58265a2
Robert Smith <robert.smith160@icloud.com>
2016-12-02 17:16:18 -0700

    "Adds show functionality iteration 4"

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,4 +1,15 @@
 class BikeShareApp < Sinatra::Base
-  
+
+  get '/trips' do
+    trips = Trip.all.order(:start_date)
+    trips = trips.each_slice(30)
+    count = 0
+    trips_hash = trips.group_by do |trip|
+      trip[count]
+      count += 1
+    end
+    @trips = trips_hash[params["page"].to_i].flatten
+    erb :'trips/show'
+  end
 
 end
---
+++ spec/features/user_sees_trip_page_spec.rb
@@ -0,0 +1,36 @@
+require_relative "../spec_helper"
+
+
+describe "trip/paths" do
+  describe "/trips" do
+    it "should only show the first thirty trips" do
+      count = 0
+      duration = 100
+      35.times do |create_trips|
+        Trip.create(id: count += 1, duration: duration += 10, start_date: "8/29/2013 9:08")
+      end
+
+      visit("/trips")
+      expect(Trip.all.count).to eq (35)
+
+      expect(page).to have_content("Trip Id")
+    end
+
+    it "should present the first thirty trips organized by date" do
+      count = 0
+      duration = 100
+      day = 0
+      35.times do |create_trips|
+        Trip.create(id: count += 1, duration: duration += 10, start_date: "8/#{day += 1}/2013")
+      end
+      visit("/trips")
+
+      # save_and_open_page
+      expect(page).to_not have_content("Trip Id: 31")
+      expect(Trip.first.start_date).to eq("8/1/2013")
+      thirtieth_trip = Trip.find(30)
+      expect(thirtieth_trip.start_date).to eq("8/30/2013")
+    end
+
+  end
+end

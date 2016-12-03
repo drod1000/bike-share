1bad53e878dd208093e043d71a0aca9d8704fc7d
Daniel Rodriguez <drod2793@gmail.com>
2016-12-03 09:17:57 -0700

    Adds update functionality for stations

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -8,6 +8,11 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/new"
   end
 
+  get '/stations/:id/edit' do |id|
+    @station = Station.find(id)
+    erb :"stations/edit"
+  end
+
   get '/stations/:id' do |id|
     @station = Station.find(id)
     erb :"stations/show"
@@ -16,5 +21,10 @@ class BikeShareApp < Sinatra::Base
   post '/station' do
     station = Station.create(params["station"])
     redirect :"stations/#{station.id}"
+  end
+
+  put '/stations/:id' do |id|
+    Station.update(id.to_i, params[:station])
+    redirect "/stations/#{id}"
   end
 end

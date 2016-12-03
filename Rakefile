de83a69cee9fc3eb70658fe1a2f5dbe8e67eb781
Robert Smith <robert.smith160@icloud.com>
2016-12-02 17:16:18 -0700

    "Updates seed and spec_helper file"

--- Rakefile
+++ Rakefile
@@ -3,3 +3,15 @@ Bundler.require
 
 require "sinatra/activerecord/rake"
 
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+RSpec.configure do |c|
+  c.before(:all) do
+    DatabaseCleaner.clean
+  end
+  c.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+desc "Import CSV to table"
+task :import do
+ create_trips
+end
+
+def create_trips
+ CSV.parse('db/csv/trip.csv', :headers=> true) do |row|
+
+   Trip.create({duration: row[1], start_date: row[2], start_station_name: row[3], start_station_id: row[4], end_date: row[5], end_station_name: row[6], end_station_id: row[7], bike_id: row[8], subscription_type: row[9], zip_code: row[10]})
+ end
+ puts "Imported Trips to Table."
+end

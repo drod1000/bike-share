cc261fb3cf204723efdca4534f4925182c684972
Brett Rivers <riverswb@gmail.com>
2016-12-04 13:39:12 -0700

    finished with feature testing I do believe

--- spec/features/conditions/user_sees_conditions_for_id_spec.rb
+++ spec/features/conditions/user_sees_conditions_for_id_spec.rb
@@ -3,13 +3,15 @@ require_relative '../../spec_helper'
 describe "Conditions/id" do
   describe "when a user visits /conditions/id" do
     it "they see a welcome message with the date" do
-      Condition.create(date: '01 DEC 2000', precipitation: 5,
-                       max_temperature: 50, min_temperature: 20,
-                       mean_temperature: 35, mean_visibility: 10,
-                       mean_wind_speed: 20, mean_humidity: 20)
+      condition = Condition.create(date: '12/01/2000', precipitation: 5,
+                                   max_temperature: 50, min_temperature: 20,
+                                   mean_temperature: 35, mean_visibility: 10,
+                                   mean_wind_speed: 20, mean_humidity: 20)
       visit('/conditions/1')
 
-      expect(page).to have_content("Conditions")
+      within("h1") do
+        expect(page).to have_content("Conditions for #{condition.date}")
+      end
     end
   end
 end

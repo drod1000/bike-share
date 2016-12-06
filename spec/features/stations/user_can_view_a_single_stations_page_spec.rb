5e2de18825ec45ee405ce599763b110cd6b5fad2
Daniel Rodriguez <drod2793@gmail.com>
2016-12-06 11:46:15 -0700

    Adds stations dashboard: date not formatted

--- spec/features/stations/user_can_view_a_single_stations_page_spec.rb
+++ spec/features/stations/user_can_view_a_single_stations_page_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../spec_helper'
 
 describe "When a user visits '/stations/id_number" do
-  it "they can see the station's name" do
+  it "they can see the that stations's info" do
     Station.create(name: "Station 1", dock_count: 5, city: "Denver", installation_date: "12/2/2016")
 
     visit '/stations/1'
@@ -9,5 +9,10 @@ describe "When a user visits '/stations/id_number" do
     within "h1" do
       expect(page).to have_content("Station 1")
     end
+
+    expect(page).to have_content("Dock Count: 5")
+    expect(page).to have_content("City: Denver")
+    expect(page).to have_content("Installation Date: 2016-02-12")
+
   end
 end

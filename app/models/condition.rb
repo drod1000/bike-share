8e59b7070669a2842dce7003c74d43b5902900ba
Brett Rivers <riverswb@gmail.com>
2016-12-07 09:47:48 -0700

    updating github

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -17,6 +17,10 @@ class Condition < ActiveRecord::Base
     where(max_temperature_f: base_temp...(base_temp + 10))
   end
 
+  def self.trips_per_day
+    Trip.group(:start_date).count
+  end
+
   def self.average_max_temp_trips(base_temp)
     # dates = Condition.max_temperature_range(base_temp).map {|c| c.date}
     # trip_dates = dates.map {|d| Trip.where(start_date: d)}
@@ -24,17 +28,30 @@ class Condition < ActiveRecord::Base
     # avg = (trip_count.reduce(:+) / trip_count.count)
     # avg
     found = []
+    # trips_per_day = Trip.group(:start_date).count
     Condition.max_temperature_range(base_temp).find_each do |trip|
-      found << Trip.where(start_date: trip.date)
+      Trip.where(start_date: trip.date).find_each do |date|
+        found << trips_per_day[date.start_date]
+      end
+      # Trip.where(start_date: trip.date).average("id")
     end
-    found
+    found.reduce(:+) / found.count
+    # trip_count = found.map {|date| date.count}
+    # trip_count.reduce(:+) / trip_count.count
   end
 
   def self.highest_max_temp_trips(base_temp)
-    dates = Condition.max_temperature_range(base_temp).map {|c| c.date}
-    trip_dates = dates.map {|d| Trip.where(start_date: d)}
-    trip_count = trip_dates.map {|c| c.count}
-    trip_count.max
+    # dates = Condition.max_temperature_range(base_temp).map {|c| c.date}
+    # trip_dates = dates.map {|d| Trip.where(start_date: d)}
+    # trip_count = trip_dates.map {|c| c.count}
+    # trip_count.max
+    # trips_per_day = Trip.group(:start_date).count
+    Condition.max_temperature_range(base_temp).find_each do |trip|
+      Trip.where(start_date: trip.date).find_each do |date|
+        found = ((Trip.group(:start_date).count)[date.start_date])
+      end
+    end
+    pry
   end
 
   def self.lowest_max_temp_trips(base_temp)

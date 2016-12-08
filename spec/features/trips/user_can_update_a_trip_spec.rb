e30d14cc46620babfafc1297befa039b619e089e
Robert Smith <robert.smith160@icloud.com>
2016-12-07 19:03:53 -0700

    "Updates trips/:id/edit with delete button. All tests passing."

--- spec/features/trips/user_can_update_a_trip_spec.rb
+++ spec/features/trips/user_can_update_a_trip_spec.rb
@@ -32,7 +32,7 @@ describe "when a user visits '/trips/edit'" do
     expect(current_path).to eql('/trips/4069')
   end
 
-  xit "the user should also be able to delete a trip" do
+  it "the user should also be able to delete a trip" do
     Trip.create(id: 4069, duration: 174, start_date: "2013-09-01 00:00:00 UTC", start_station_name: "2nd at South Park", start_station_id: 64, end_date: "2013-09-30 00:00:00 UTC", end_station_name: "2nd at South Park", end_station_id: 64, bike_id: 288, subscription_type: "Subscriber", zip_code: 94114, created_at: "2016-12-02 23:59:08", updated_at: "2016-12-02 23:59:08")
 
     visit("/trips/#{Trip.first.id}/edit")
